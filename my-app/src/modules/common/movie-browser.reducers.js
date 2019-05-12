77a3017e0176fa2f2a6bfa46cb0687ceae75ac11
Piotr Badura <bajdurato@gmail.com>
2019-05-12 14:08:41 +0200

    Poczatek react

---
+++ my-app/src/modules/common/movie-browser.reducers.js
@@ -0,0 +1,18 @@
+import {
+    combineReducers
+} from 'redux';
+import {
+    createReducer
+} from '../common/redux.helpers';
+
+const movieModalReducer = createReducer({
+    isOpen: false
+}, {
+
+});
+
+const movieBrowserReducer = combineReducers({
+    movieModal: movieModalReducer
+});
+
+export default movieBrowserReducer;
